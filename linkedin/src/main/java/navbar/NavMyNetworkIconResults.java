ccb9668a579336a9a799f808844988291bb4eea1
Nuard <nuard@atabekyan.com>
2018-05-03 21:35:31 +0400

    add test

---
+++ linkedin/src/main/java/navbar/NavMyNetworkIconResults.java
@@ -0,0 +1,11 @@
+package navbar;
+
+import org.openqa.selenium.WebDriver;
+
+public class NavMyNetworkIconResults extends NavBarResults {
+
+		public NavMyNetworkIconResults (WebDriver driver) {
+			super(driver);
+		}
+	public static final String MyNetworkPageXpath="//h3[@class='mn-tabbed-pymk__header Sans-19px-black-70%-open']";
+}
